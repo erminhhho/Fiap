e3c24dfada972d12b0eb2606bb1c6703e2f98ea2
erminhhho <erminhhho@outlook.com>
2025-06-04 16:23:48 -0300

    Remove obsolete test HTML files for data persistence and final integration tests

--- debug_console.js
+++
@@ -1,5 +0,0 @@
-console.log('=== DEBUG DADOS PERSONAL ===');
-console.log('formStateManager.formData.personal:', JSON.stringify(window.formStateManager.formData.personal, null, 2));
-console.log('autor_nome[]:', window.formStateManager.formData.personal['autor_nome[]']);
-console.log('autor_cpf[]:', window.formStateManager.formData.personal['autor_cpf[]']);
-console.log('autor_idade[]:', window.formStateManager.formData.personal['autor_idade[]']);
